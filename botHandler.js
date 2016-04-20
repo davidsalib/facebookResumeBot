ea6490ad1747b3b503ed2f7fe643bf7cac9c23d5
David Salib <david.salib@centricate.ca>
2016-04-19 20:49:43 -0400

    jobs experience

--- botHandler.js
+++ botHandler.js
@@ -2,127 +2,133 @@ var request = require('request');
 var token = "EAAYQyuiljoABABWfDx8QZAtZCkvpkx9TbZAkmv8MehxZAF2OgeQviRxz6nWNW0gxKgMq50vDEMIUY3j2P8t05wq7rOHEbVjCFiRANvetqAgnWqrNX9ZAPpcTrTRgZCi3T1oZBeDPV9hVheJeGAc18E49KiDMkRsRGGTt8CFxiyNyQZDZD";
 
 var resume = {
-  name: "David Salib",
-  jobs: [{
-    id: "SIE",
-    img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg",
-    company: "Sony Interactive Entertainment",
-    role: "Software Engineer",
-    startDate: "January 2016",
-    endDate: "April 2016",
-    desc: [
-      "I worked on the PlayStation Store as well as the PlayStation Video App for iOS and Android."
-    ],
-    highlightAchievement: "On launch team for PlayStation Video for Android"
-  }, {
-    id: "FAO",
-    img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg",
-    company: "Frank& Oak",
-    role: "Web Developer",
-    startDate: "May 2015",
-    endDate: "September 2015",
-    desc: [
-      "Developed ecommerce and API tools to support one of North America's fastest growing fashion startups."
-    ],
-    highlightAchievement: "David proposed and developed a customer tool that replaced Frank & Oak's previous order fulfillment process, improving efficiency by 200%"
-  }],
-  education: {
-    institution: "University of Waterloo",
-    degree: "Computer Science",
-    startYear: 2014,
-    endYear: 2019
-  },
-  projects: [{
-      title: "Catena",
-      img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
-  },{
-    title: "FHTSystem",
-    img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
-  },{
-    title: "Pluto",
-    img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
-  }]
-}
+    name: "David Salib",
+    jobs: [{
+        id: "SIE",
+        img: "http://media.psu.com/media/articles/image/PlayStation%20Logo.jpg",
+        company: "Sony Interactive Entertainment",
+        role: "Software Engineer",
+        startDate: "January 2016",
+        endDate: "April 2016",
+        desc: [
+            "I worked on the PlayStation Store as well as the PlayStation Video App for iOS and Android."
+        ],
+        highlightAchievement: "On launch team for PlayStation Video for Android"
+    }, {
+        id: "FAO",
+        img: "https://edge.frankandoak.com/media/stylenote/uploads/2015/03/STORE1.jpg",
+        company: "Frank& Oak",
+        role: "Web Developer",
+        startDate: "May 2015",
+        endDate: "September 2015",
+        desc: [
+            "Developed e-commerce and API tools to support one of North America's fastest growing fashion startups."
+        ],
+        highlightAchievement: "David proposed and developed a customer tool that replaced Frank & Oak's previous order fulfillment process, improving efficiency by 200%"
+    }],
+    education: {
+        institution: "University of Waterloo",
+        degree: "Computer Science",
+        startYear: 2014,
+        endYear: 2019
+    },
+    projects: [{
+        title: "Catena",
+        img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
+    }, {
+        title: "FHTSystem",
+        img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
+    }, {
+        title: "Pluto",
+        img: "https://www.savannahstate.edu/academic-affairs/images/thailand_pic.jpg"
+    }]
+};
 
 var botHandler = function (sender, text) {
-  text = text.toLowerCase();
-  if (text == "experience") {
-    sendJobExperience(sender);
-  } else {
-    sendTextMessage(sender, "Hey, nice to meet you :) Would you like to learn about David Salib? You can ask about Projects, Job Experience, Education, and his Interests. Go ahead, try it!");
-  }
-}
+    text = text.toLowerCase();
+    if (text == "experience") {
+        sendJobExperience(sender);
+    } else {
+        sendTextMessage(sender, "Hey, nice to meet you :) Would you like to learn about David Salib? You can ask about Projects, Job Experience, Education, and his Interests. Go ahead, try it!");
+    }
+};
 
 function sendTextMessage(sender, text) {
-  messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
+    messageData = {
+        text: text
     }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 function sendJobExperience(sender) {
-  var payload = [];
-  var jobs = resume.jobs;
-  for (var i = 0; i < jobs.length; i++) {
-    var item =  {
-      title: jobs[i].role,
-      subtitle: jobs[i].company,
-      image_url: jobs[i].img,
-      buttons: [{
-        type: "postback",
-        title: "Tell me more",
-        payload: "@"+jobs[i].id+":more"
-      }, {
-        type: "postback",
-        title: "What was your biggest achievement at " + jobs[i].company,
-        payload: "@"+jobs[i].id+":highlight"
-      }]
+    var payload = [];
+    var jobs = resume.jobs;
+    for (var i = 0; i < jobs.length; i++) {
+        var item = {
+            title: jobs[i].role,
+            subtitle: jobs[i].company,
+            image_url: jobs[i].img,
+            buttons: [{
+                type: "postback",
+                title: "Tell me more",
+                payload: "@" + jobs[i].id + ":more"
+            }, {
+                type: "postback",
+                title: "Highlight Achievement",
+                payload: "@" + jobs[i].id + ":highlight"
+            }, {
+                type: "postback",
+                title: "Technologies Used",
+                payload: "@" + jobs[i].id + ":technologies"
+            }]
+        };
+        payload.push(item);
+    }
+
+    sendCards(sender, payload);
+}
+
+
+function sendCards(sender, payload) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": payload
+            }
+        }
     };
-    console.log("item:-------"+JSON.stringify(item));
-    payload.push(item);
-  }
 
-  console.log(payload);
-
-  messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": payload
-      }
-    }
-  };
-
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 module.exports = botHandler;
