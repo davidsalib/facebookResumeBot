a1215497c54d962c5bd1c34b3e2c0b1ff645b8e1
David Salib <david.salib@centricate.ca>
2016-05-13 17:46:36 -0400

    fix

--- botHandler.js
+++ botHandler.js
@@ -32,7 +32,7 @@ var botHandler = function (sender, payload, type) {
             else if (payload.indexOf('bye') > -1 || payload.indexOf('later') > -1) {
                 sendTextMessage(sender, "It was great talking to you! Have a fantastic day :)");
             } else {
-                sendTextMessage(sender, "jdudfaSJJdSLEod DJDJe SJSJS... That's how it feels when you send me something I don't understand :( How about trying to ask about David's Job Experience, Skills, Projects, Education, or Interests? \n\n...Sorry, sometimes I get a bit sad that David didn't build any natural language processing for me :'( (he said he might use wit.ai in the future to improve me!) ");
+                sendTextMessage(sender, "jdudfaSJJdSLEod DJDJe SJSJS... That's how it feels when you send me something I don't understand :( Try asking about David's Job Experience, Skills, Projects, Education, or Interests. \n\n...Sorry, sometimes I get a bit sad that David didn't build me natural language processing :'( (he said he might use wit.ai in the future to improve me!) ");
             }
         } else if (type == "postback") {
             switch (payload.action) {
